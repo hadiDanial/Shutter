a263b875b120d3ff8082fd4612336a47f9632318
hadiDanial <hadidanial97@gmail.com>
2021-03-24 01:59:13 +0200

    Update ScreenshotData.cs
    Rename `name` to `time`, remove `path`, add `BitmapSource` (might be useless/removed)

--- ScreenshotApp/Code/ScreenshotData.cs
+++ ScreenshotApp/Code/ScreenshotData.cs
@@ -1,22 +1,24 @@
 ﻿using System.Drawing;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace ScreenshotApp.Code
 {
     struct ScreenshotData
     {
         public Bitmap captureBitmap;
+        public BitmapSource bitmapSource;
         public Graphics captureGraphics;
         public ImageSource imageSource;
-        public string name, path;
+        public string time;
 
-        public ScreenshotData(Bitmap captureBitmap, Graphics captureGraphics, ImageSource imageSource, string name, string path)
+        public ScreenshotData(Bitmap captureBitmap, BitmapSource bitmapSource, Graphics captureGraphics, ImageSource imageSource, string time)
         {
             this.captureBitmap = captureBitmap;
+            this.bitmapSource = bitmapSource;
             this.captureGraphics = captureGraphics;
             this.imageSource = imageSource;
-            this.name = name;
-            this.path = path;
+            this.time = time;
         }
     }
 }
